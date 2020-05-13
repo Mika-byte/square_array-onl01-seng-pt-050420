b1d95aaded319cd0f8ba5703aa4802c130a76485
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-13 22:07:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,15 @@
 require "pry"
-# def square_array(numbers)
-#   new_array = []
-#   numbers.each do |number|
-#   new_array << number * number
-#   end
-#   return new_array
-# end
 def square_array(numbers)
-  
-  numbers.collect do |number|
-    number * number
+  new_array = []
+  numbers.each do |number|
+  new_array << number * number
   end
- 
+  return new_array
 end
+# def square_array(numbers)
+  
+#   numbers.collect do |number|
+#     number * number
+#   end
+ 
+# end
